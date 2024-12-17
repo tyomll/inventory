ed7281cf66dcc6df1dbbcfa764d165438c3c9520
Artyom Hovsepyan <artyomhovsepyan135@gmail.com>
2024-12-18 00:29:43 +0400

    Added cors options

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*",
+    credentials: true,
+  });
+
+  
   await app.listen(3000, () => {
     console.log("Server started listening on port 3000");
   });
